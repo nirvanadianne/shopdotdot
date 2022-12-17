d45af60d05bbff373edfbb6953c2121a4fb5c40d
nirvanadianne <nirvanadiannecruz@icloud.com>
2022-12-17 02:40:10 +0000

    created

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shopdotdot</a>
+      </header>
+      <main>product list</main>
+    </div>
+  );
+}
+
+export default App;
