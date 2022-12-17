9220680ad4cb646263fc3edc41859c1a2e0a9f55
nirvanadianne <nirvanadiannecruz@icloud.com>
2022-12-17 04:38:00 +0000

    adding new in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">shopdotdot</a>
       </header>
-      <main>product list</main>
+      <main>
+        <h1> Shop New In</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  width="279"
+                  height="350"
+                />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>£{product.price}</strong>
+                </p>
+                <button>add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+const data = {
+  products: [
+    {
+      name: 'Hermes Kelly',
+      slug: 'product-001',
+      category: 'bags',
+      brand: 'Hermes',
+      image: '/images/p1.jpg',
+      price: 16000,
+      description: 'Hermes Kelly 25, black, palladium hardware',
+    },
+    {
+      name: 'Hermes Birkin',
+      slug: 'product-002',
+      category: 'bags',
+      brand: 'Hermes',
+      image: '/images/p2.jpg',
+      price: 15000,
+      description: 'Hermes Birkin 25, etain, palladium hardware',
+    },
+
+    {
+      name: 'Chanel Tweed Classic Flap',
+      slug: 'product-003',
+      category: 'bags',
+      brand: 'Chanel',
+      image: '/images/p3.jpg',
+      price: 5000,
+      description:
+        'Chanel Tweed Classic flap, small, Red and Blue, gold hardware',
+    },
+    {
+      name: 'Chanel Classic Flap',
+      slug: 'product-004',
+      category: 'bags',
+      brand: 'Chanel',
+      image: '/images/p4.jpg',
+      price: 8000,
+      description: 'Chanel Classic Flap, Medium, Pink, gold hardware',
+    },
+  ],
+};
+
+export default data;
